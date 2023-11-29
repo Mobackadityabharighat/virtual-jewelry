a03aabebfdaabaaf6df2c3dc1a73a7cea032b410
subhama <subhama@moback.com>
2023-11-29 20:11:00 +0530

    trial

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-
-
+#!/usr/bin/env python
+from importlib import import_module
+import os
 from flask import Flask, render_template, Response
-import cv2
-app=Flask(__name__)
-camera = cv2.VideoCapture(0)
 
-#
-def gen_frames():
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+
+# Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    yield b'--frame\r\n'
     while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            detector=cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
-            eye_cascade = cv2.CascadeClassifier('Haarcascades/haarcascade_eye.xml')
-            faces=detector.detectMultiScale(frame,1.1,7)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-             #Draw the rectangle around each face
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = frame[y:y+h, x:x+w]
-                eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-                for (ex, ey, ew, eh) in eyes:
-                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        frame = camera.get_frame()
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-# if __name__=='__main__':
-#     app.run(debug=True)
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
