2c62d146d67f07becbeea3db1a34f6c9b6f3603d
vinod_r <vinod.raj@kornferry.com>
2023-12-06 18:58:05 +0530

    feat: updates to include client server model

---
+++ Client.py
@@ -0,0 +1,56 @@
+import cv2
+import socket
+import struct
+import pickle
+import imutils
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.5.128', 8485))  # Replace with your server's IP and port
+
+cam = cv2.VideoCapture(0)
+img_counter = 0
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+    ret, frame = cam.read()
+
+    frame = imutils.resize(frame, width=320)
+    frame = cv2.flip(frame, 180)
+
+    result, image = cv2.imencode('.jpg', frame, encode_param)
+    data = pickle.dumps(image, 0)
+    size = len(data)
+
+    client_socket.sendall(struct.pack(">L", size) + data)
+
+    # Receive augmented feed from the server
+    data = b""
+    payload_size = struct.calcsize(">L")
+    while len(data) < payload_size:
+        data += client_socket.recv(4096)
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    # Decode and display the augmented frame
+    augmented_frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    augmented_frame = cv2.imdecode(augmented_frame, cv2.IMREAD_COLOR)
+    #cv2.putText(augmented_frame ,"Client", (100,100),cv2.FONT_HERSHEY_SIMPLEX ,1,(255,0,0),2)
+    cv2.imshow('Augmented Feed', augmented_frame)
+
+    img_counter += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+client_socket.close()
---
+++ server.py
@@ -0,0 +1,67 @@
+# Import the required modules
+
+import socket
+import cv2
+import pickle
+import struct
+import AugumentationModule as aug
+
+def SendFeed(frame):
+    feed =augmentation.Augument(frame, jewel_img)
+
+    # Encode the augmented frame
+    _, encoded_frame = cv2.imencode(".jpg", feed)
+    frame_data = pickle.dumps(encoded_frame, protocol=0)
+
+    # Send the size of the frame data
+    size = struct.pack(">L", len(frame_data))
+    conn.sendall(size)
+
+    # Send the frame data
+    conn.sendall(frame_data)
+
+
+
+HOST = ''
+PORT = 8485
+augmentation = aug.NecklaceAugumentation()
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn,addr=s.accept()
+
+data = b""
+payload_size = struct.calcsize(">L")
+
+print("payload_size: {}".format(payload_size))
+
+jewel_img = cv2.imread("Images/alok2.png")
+#jewel_img = cv2.imread("Images/alok2.png")
+
+while True:
+    while len(data) < payload_size:
+        data += conn.recv(4096)
+        if not data:
+            cv2.destroyAllWindows()
+            conn, addr = s.accept()
+            continue
+    # receive image row data form client socket
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    # unpack image using pickle 
+    frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    SendFeed(frame)
+
+    cv2.waitKey(1)
+
